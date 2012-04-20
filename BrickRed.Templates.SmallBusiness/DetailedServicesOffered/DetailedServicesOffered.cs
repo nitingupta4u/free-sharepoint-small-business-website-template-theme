d5ef20edbf00e3883f61357693f8c2275dadc80f
tsg <tsg@brickred.com>
2012-04-20 13:34:49 +0000

    Updated Services Webpart

---
+++ BrickRed.Templates.SmallBusiness/DetailedServicesOffered/DetailedServicesOffered.cs
@@ -0,0 +1,67 @@
+﻿/*
+===========================================================================
+Copyright (c) 2010 BrickRed Technologies Limited
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===========================================================================
+*/
+
+
+using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+using Microsoft.SharePoint.WebPartPages;
+
+namespace BrickRed.Templates.SmallBusiness.DetailedServicesOffered
+{
+    [ToolboxItemAttribute(false)]
+    public class DetailedServicesOffered : Microsoft.SharePoint.WebPartPages.WebPart
+    {
+        // Visual Studio might automatically update this path when you change the Visual Web Part project item.
+        private const string _ascxPath = @"~/_CONTROLTEMPLATES/BrickRed.Templates.SmallBusiness/DetailedServicesOffered/DetailedServicesOfferedUserControl.ascx";
+
+
+        #region WebPart Property
+
+        [WebBrowsable(true),
+        Category("BrickRed.SmallBusiness"),
+        Personalizable(PersonalizationScope.Shared),
+        WebPartStorage(Storage.Shared),
+        WebDisplayName("Show Services Image"),
+        WebDescription("Please check if the images are to be shown")]
+        public bool ShowServicesImages
+        {
+            get { return DetailedServicesOfferedUserControl.ShowImages; }
+            set { DetailedServicesOfferedUserControl.ShowImages = value; }
+        }
+
+        #endregion
+
+        protected override void CreateChildControls()
+        {
+            Control control = Page.LoadControl(_ascxPath);
+            Controls.Add(control);
+        }
+    }
+}
